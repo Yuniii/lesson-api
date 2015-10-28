84ac62719b1f89b148b49b18e421297c177cf192
CodinCat <a55951234@gmail.com>
2015-10-28 15:16:42 +0800

    ok

--- server.js
+++ server.js
@@ -14,8 +14,42 @@ app.get('/:course/:lesson', function(req, res) {
         if (err !== null) {
             return res.send('no result');
         }
+        var quizJson = [],
+            aQuiz = {};
+
         parser.parseString(data, function(err, result) {
-            res.send(result.project.file);
+            for (var i = result.project.file.length - 1; i >= 0; i--) {
+                var obj = result.project.file[i],
+                    name = obj.path[0],
+                    content = obj.content[0];
+
+                var title = name.substr(0, name.length - 5),
+                    type = name.substr(-5, 5);
+                
+                if (typeof aQuiz[title] === 'undefined') {
+                    aQuiz[title] = {};
+                    aQuiz[title].title = title;
+                }
+
+                if (type === '.cond') {
+                    aQuiz[title].stdout = content;
+                }
+                else if (type === '.html') {
+                    aQuiz[title].description = content;
+                }
+                else if (type === '.java') {
+                    aQuiz[title].ans = content;
+                }
+                else if (type === '.part') {
+                    aQuiz[title].part = content;
+                }
+            }
+
+            for (var title in aQuiz) {
+                quizJson.push(aQuiz[title]);
+            }
+
+            res.send(quizJson);
         });
     });
 });
